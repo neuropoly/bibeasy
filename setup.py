54cd8d1ba05d998926a0de121288bb1126c903e7
kalum.ost <kalum.ost@polymtl.ca>
2025-01-17 15:35:50 -0500

    Updated SetupTools entry points to correctly refer to the updated CLI call

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'bibeasy = bibeasy.scripts.bibeasy_cli:main',
-            ]
-        }
-    )
+            'bibeasy = bibeasy.scripts:bibeasy_cli',
+        ]
+    }
+)
